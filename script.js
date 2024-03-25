3ba54311d2c8b5e1179cd5481ced912326460e0e
Arnold Juma <+>
2024-03-25 13:31:17 +0300

    done

--- script.js
+++ script.js
@@ -9,17 +9,8 @@ async function checkWeather(city){
 
     const response =await fetch(apiUrl + city +`&appid=${apiKey}`);
    
-   
-    if (response.status = 404){
-        document.querySelector('.error').style.display ="block";
-        document.querySelector('.weather').style.display ="block";
-
-    }
-    else{
 
         var data = await response.json();
-
-
 
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°C";
@@ -41,10 +32,6 @@ async function checkWeather(city){
         else if(data.weather[0].main=="Mist"){
             weatherIcon.src="images/mist.png";
         }
-    
-        document.querySelector(".weather").style.display ="block";
-        document.querySelector(".error").style.display ="none";
-    }
    
    
 }    
